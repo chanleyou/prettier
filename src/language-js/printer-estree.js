e5025f11d32cf00d27ed25a74f8f47f49b05d47d
Brandon Mills <mills.brandont@gmail.com>
2018-11-02 00:11:02 -0400

    Break lines before binary operators

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -5203,16 +5203,10 @@ function printBinaryishExpressions(
     }
 
     const shouldInline = shouldInlineLogicalExpression(node);
-    const lineBeforeOperator = node.operator === "|>";
 
     const right = shouldInline
       ? concat([node.operator, " ", path.call(print, "right")])
-      : concat([
-          lineBeforeOperator ? softline : "",
-          node.operator,
-          lineBeforeOperator ? " " : line,
-          path.call(print, "right")
-        ]);
+      : concat([softline, node.operator, " ", path.call(print, "right")]);
 
     // If there's only a single binary expression, we want to create a group
     // in order to avoid having a small right part like -1 be on its own line.
