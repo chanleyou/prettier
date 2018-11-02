a85f417c984fcc406bcb579bf05f2875d1cd6775
Brandon Mills <mills.brandont@gmail.com>
2018-11-02 00:11:02 -0400

    Break lines before binary operators

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -5843,9 +5843,6 @@ function printBinaryishExpressions(
 
     const shouldInline = shouldInlineLogicalExpression(node);
     const lineBeforeOperator =
-      (node.operator === "|>" ||
-        node.type === "NGPipeExpression" ||
-        (node.operator === "|" && options.parser === "__vue_expression")) &&
       !hasLeadingOwnLineComment(options.originalText, node.right, options);
 
     const operator = node.type === "NGPipeExpression" ? "|" : node.operator;
